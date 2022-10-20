cf71481e187b0e5fbf3755d925ab507c74d0a06f
OsamaSHokry <67597530+OsamaSHokry@users.noreply.github.com>
2022-10-20 14:03:46 +0200

    Delete Deploy directory

--- Deploy/Pages/A1_Home.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from Deploy import my_var
-
-st.subheader("Home Page")
-st.write(my_var)
-
-test_var = 'Test Var'
--- Deploy/Pages/A2_EDA.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from Pages.A1_Home import test_var
-
-st.subheader("EDA Page")
-st.write(test_var)
