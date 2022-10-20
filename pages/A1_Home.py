a4df67918045949115ebc79b3af37b1e59a54100
OsamaSHokry <67597530+OsamaSHokry@users.noreply.github.com>
2022-10-20 14:06:35 +0200

    Add files via upload

---
+++ pages/A1_Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from Deploy import my_var
+
+st.subheader("Home Page")
+st.write(my_var)
+
+test_var = 'Test Var'
---
+++ pages/A2_EDA.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from pages.A1_Home import test_var
+
+st.subheader("EDA Page")
+st.write(test_var)
