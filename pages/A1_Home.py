c7b3a55ab114994530d803afdab636f533eb6e8c
OsamaSHokry <67597530+OsamaSHokry@users.noreply.github.com>
2022-10-20 18:05:13 +0200

    Update A1_Home.py

--- pages/A1_Home.py
+++ pages/A1_Home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from Deploy import my_var
-# from Deploy import data
+from Deploy import data_0
 
 st.subheader("Home Page")
 st.write(my_var)
-# st.write(data.head())
+st.write(data_0.head())
 
 test_var = 'Test Var'
