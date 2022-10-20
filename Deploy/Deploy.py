f2bdeddf5969a53ebe04ef2f2feeddbd4b46a447
OsamaSHokry <67597530+OsamaSHokry@users.noreply.github.com>
2022-10-20 13:32:32 +0200

    Add files via upload

---
+++ Deploy/Deploy.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+import plotly.express as px
+my_var = " my variable says Hello"
+
+def main():
+    st.title("Streamlit Muli-Page")
+    st.subheader("Main Page")
+    st.write(my_var)
+
+    choice = st.sidebar.selectbox("SubMenu",["Pandas","Tenserflow"])
+    if choice == "Pandas":
+        st.subheader("Pandas")
+
+if __name__ == '__main__':
+    main()
---
+++ Deploy/Pages/A2_EDA.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from Pages.A1_Home import test_var
+
+st.subheader("EDA Page")
+st.write(test_var)
